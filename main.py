b5eb8162afa715a6ed28a9295946e8f9be293724
Slow1 <skreamlight@gmail.com>
2023-01-11 15:04:19 -0400

    added url

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+from bs4 import BeautifulSoup
+import requests 
+import pandas as pd
+
+url = 'https://resultados.as.com/resultados/futbol/mundial/2022/ranking/'
+
 def run():
     pass
-
 
 if __name__ == '__main__':
     run()
